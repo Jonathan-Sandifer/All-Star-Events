e57d2db9d88d24836db2a3277a9460bcf7865f28
Shane <ssiebler21@gmail.com>
2022-05-31 17:52:28 -0600

    added account project and app

--- accounts/account_project/urls.py
+++ accounts/account_project/urls.py
@@ -1,4 +1,4 @@
-"""accounts_project URL Configuration
+"""account_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
