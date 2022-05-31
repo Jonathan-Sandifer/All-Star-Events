5cf26f57692810f0dac0ddae7ef7f4ef289f8ac4
Colton <coltstecker@gmail.com>
2022-05-31 18:42:29 -0500

    Daily journal

--- parks/parks_rest/models.py
+++ parks/parks_rest/models.py
@@ -6,3 +6,8 @@ class Park(models.Model):
     name = models.CharField(max_length=250)
     description = models.CharField(max_length=600)
     picture_url = models.URLField(null=True)
+
+class User(models.Model):
+    id = models. PositiveSmallIntegerField()
+    name = models.CharField(max_length=50)
+    
