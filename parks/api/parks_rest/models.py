1ba588155d8db640ac63847205e264dfc4590b0d
Colton <coltstecker@gmail.com>
2022-06-01 13:24:01 -0500

    updated park model

--- parks/api/parks_rest/models.py
+++ parks/api/parks_rest/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Park(models.Model):
-    id = models.PositiveSmallIntegerField()
     name = models.CharField(max_length=250)
     description = models.CharField(max_length=600)
     picture_url = models.URLField(null=True)
