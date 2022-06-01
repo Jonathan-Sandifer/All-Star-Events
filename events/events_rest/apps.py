dbf789920bb2004d3afb312ff9411ca896f9fed2
Kieran <kr292587@gmail.com>
2022-06-01 09:03:34 -0700

    almost finished yml, most containers up

--- events/events_rest/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EventsRestConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'events_rest'
---
+++ parks/api/parks_rest/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Park(models.Model):
+    id = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=600)
+    picture_url = models.URLField(null=True)
