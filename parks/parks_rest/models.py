8761262f5e0bdce53e8a6901e49082c14ef9a70a
Jonathan Sandifer <jaysandifer5000@gmail.com>
2022-06-01 13:47:48 -0500

    Deleted keys.py file so that it can be put inside a git ignore file

--- parks/parks_rest/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Park(models.Model):
-    id = models.PositiveSmallIntegerField()
-    name = models.CharField(max_length=250)
-    description = models.CharField(max_length=600)
-    picture_url = models.URLField(null=True)
