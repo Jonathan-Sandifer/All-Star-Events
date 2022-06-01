8c819dbde70b45b5036118878fd1fca2b6025922
Colton <coltstecker@gmail.com>
2022-06-01 18:22:49 -0500

    Basic main page up

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import MainPage from './MainPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MainPage />
       
     </div>
   );
---
+++ ghi/src/MainPage.js
@@ -0,0 +1,15 @@
+function MainPage(props) {
+    return (
+      <div className="px-4 py-5 my-5 text-center">
+        <h1 className="display-5 fw-bold">All Star Events</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">
+            Just four dudes missing sunlight!
+          </p>
+          
+        </div>
+      </div>
+    );
+  }
+  
+  export default MainPage;
