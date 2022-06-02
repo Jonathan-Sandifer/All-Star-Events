5da9c9384e86aa0ea53dd08d45dfa8a62ab07b34
Kieran <kr292587@gmail.com>
2022-06-02 16:01:43 -0700

    added accounts service to yml file

--- accounts/accounts_rest/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-
-# Create your models here. # still have to link app together 
---
+++ accounts/api/accounts_rest/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+# Create your models here. # still have to link app together 
