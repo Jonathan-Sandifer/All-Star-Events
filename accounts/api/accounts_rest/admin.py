213a7838d1dfa4fec009ef6066aac8baaac4e356
Shane <ssiebler21@gmail.com>
2022-06-02 19:27:02 -0600

    registered the admin to populate some dummy data

--- accounts/api/accounts_rest/admin.py
+++ accounts/api/accounts_rest/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Preferences
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+  pass
+class PreferencesAdmin(admin.ModelAdmin):
+  pass
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Preferences, PreferencesAdmin)
+
