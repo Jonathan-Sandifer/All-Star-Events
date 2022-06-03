36a567ba2f35edc1e73b09f909ffa79e65e9b5c7
Jonathan Sandifer <jaysandifer5000@gmail.com>
2022-06-03 16:41:15 -0500

    Got a picture to show for the logout page but it isnt fitting the page

--- ghi/src/components/Logout.js
+++ ghi/src/components/Logout.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logout() {
     return (
-      <div className="px-4 py-5 my-5 text-center">
+      <div className="px-4 goodbye py-5 my-5 text-center">
         <h1 className="display-5 fw-bold">You Are Now Logged Out</h1>
         <div className="col-lg-6 mx-auto">
         </div>
