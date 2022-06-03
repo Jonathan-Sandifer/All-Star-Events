c0e08ed121771911b06d18a72a24fb6aaca083f2
Colton <coltstecker@gmail.com>
2022-06-03 13:32:51 -0500

    Mainpage looks nice

--- ghi/src/components/MainPage.js
+++ ghi/src/components/MainPage.js
@@ -4,9 +4,10 @@ import Search from './Search';
 function MainPage() {
     return (
       <div className="mainpage">
-        <div className="mainpage">
-          <h1 className="display-5 fw-bold">Welcome to All Star Events</h1>
+        <div>
+          <h1 className="mainheader">Welcome to All Star Events</h1>
         </div>
+        <br />
         <Search />
         <div className="footer">
           <p>Just four dudes missing sunlight</p>
