787bfa0914fe1b019c38aface61d673fafa5aee1
Shane <ssiebler21@gmail.com>
2022-06-02 19:11:53 -0600

    added user and preference model + migrated

--- accounts/api/accounts_rest/models.py
+++ accounts/api/accounts_rest/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here. # still have to link app together 
+
+class User(models.Model):
+  first_name = models.CharField(max_length=30, unique=False)
+  last_name = models.CharField(max_length=30, unique=False)
+  user_name = models.CharField(max_length=30, unique=True )
+  email = models.EmailField(max_length=100, unique=True)
+  password = models.CharField(max_length=20, unique=True)
+  
+
+class Preferences(models.Model):
+  name = models.CharField(max_length= 50, unique=True)
+  user = models.ManyToManyField(User)
