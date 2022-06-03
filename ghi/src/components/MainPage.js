f94ec876349cc912506b53f513af8115842e18da
Colton <coltstecker@gmail.com>
2022-06-03 12:59:55 -0500

    Added a footer

--- ghi/src/components/MainPage.js
+++ ghi/src/components/MainPage.js
@@ -4,18 +4,14 @@ import Search from './Search';
 function MainPage() {
     return (
       <div className="mainpage">
-        <h1 className="display-5 fw-bold">Welcome to All Star Events</h1>
-        <div className="col-lg-6 mx-auto">
-        <Search />
-          <p className="lead mb-4">
-            Who are we?
-            <br />
-            <br />
-            
-            Just four dudes missing sunlight!
-          </p>
-          
+        <div className="mainpage">
+          <h1 className="display-5 fw-bold">Welcome to All Star Events</h1>
         </div>
+        <Search />
+        <div className="footer">
+          <p>Just four dudes missing sunlight</p>
+        </div>
+          
       </div>
     );
   }
