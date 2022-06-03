2bef4acecad9160d3825aef654b424b4d21da5b0
Kieran <kr292587@gmail.com>
2022-06-02 20:16:30 -0700

    added a lot of things, encoders, views, updated models, and working on integrating bootstrap

--- accounts/api/accounts_project/settings.py
+++ accounts/api/accounts_project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'account_project.urls'
+ROOT_URLCONF = 'accounts_project.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'account_project.wsgi.application'
+WSGI_APPLICATION = 'accounts_project.wsgi.application'
 
 
 # Database
--- accounts/api/accounts_rest/models.py
+++ accounts/api/accounts_rest/models.py
@@ -8,8 +8,14 @@ class User(models.Model):
   user_name = models.CharField(max_length=30, unique=True )
   email = models.EmailField(max_length=100, unique=True)
   password = models.CharField(max_length=20, unique=True)
+
+  def __str__(self):
+    return self.user_name
   
 
 class Preferences(models.Model):
   name = models.CharField(max_length= 50, unique=True)
-  user = models.ManyToManyField(User)
+  user = models.ManyToManyField("User", related_name="preferences")
+
+  def __str__(self):
+    return self.name
