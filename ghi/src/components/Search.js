9a8f225b71938c15378d42dafb14495b7c0ba629
Kieran <kr292587@gmail.com>
2022-06-08 14:47:53 -0700

    we now have lat and lon saved to state

--- ghi/src/components/Search.js
+++ ghi/src/components/Search.js
@@ -7,8 +7,8 @@ class SearchBar extends React.Component {
         super(props)
         this.state = {
             search: '',
-            lat: '',
-            lon: ''
+            latitude: '',
+            longitude: ''
         }
         this.handleSearch = this.handleSearch.bind(this);
         this.getLatLon = this.getLatLon.bind(this);
@@ -28,6 +28,13 @@ class SearchBar extends React.Component {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+            let lat = data[0]['lat'].toFixed(4)
+            let lon = data[0]['lon'].toFixed(4)
+            console.log(lat, lon);
+            delete data[0]['lat']
+            delete data[0]['lon']
+            this.setState({latitude: lat, longitude: lon})
+            
         }
 
     }
@@ -39,13 +46,6 @@ class SearchBar extends React.Component {
         <form action="/" method="get">
 
             <br />
-            {/* <input
-                type="text"
-                id="header-search"
-                placeholder="Enter your city"
-                name="s"
-            />
-            <button type="submit">Search</button> */}
             <div className="search-box">
             <input onChange={this.handleSearch} type="text"
             className="search-bar"
