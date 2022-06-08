42302db8f46ab68a1dfa22deddf4d072e06bf512
Shane <ssiebler21@gmail.com>
2022-06-08 14:24:46 -0600

    added parks model made migrations and hooked them up to the admin to populate data

--- parks/api/parks_rest/admin.py
+++ parks/api/parks_rest/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Park, BookmarkedPark
 
 # Register your models here.
+
+class ParkAdmin(admin.ModelAdmin):
+  pass
+class BookmarkedParkAdmin(admin.ModelAdmin):
+  pass
+
+admin.site.register(Park, ParkAdmin)
+admin.site.register(BookmarkedPark, BookmarkedParkAdmin)
