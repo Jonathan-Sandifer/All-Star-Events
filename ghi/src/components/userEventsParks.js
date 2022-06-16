b2d279853e642035d8063588850ca74b6e3e3210
Jonathan Sandifer <jaysandifer5000@gmail.com>
2022-06-15 19:03:41 -0500

    Created a working card for page

--- ghi/src/components/userEventsParks.js
+++ ghi/src/components/userEventsParks.js
@@ -6,39 +6,20 @@ import React from 'react'
 function UserEventsParks() {
     return (
       <div className="user-events-parks">
-        <br />
-        <br />
-            <div className="user-column">
-              <h1> Your Events </h1>
+        <div class="user-cards">
+          <div class="user-card">
+            <div class= "user-card-content">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Eos ducimus id ab tenetur delectus reiciendis fugit autem qui at.
+              </p>
+              <p>
+                Alias itaque praesentium eum, 
+                consequatur ducimus asperiores accusantium velit minima?
+              </p>
+              <div class="user-card-content"></div>
             </div>
-            <div className="user-column">
-              <h1>Your Parks</h1>
-            </div>
-            <div className="user-row">
-            <div class="user-card">
-              <div className="user-column">
-                <h1>Falling in Reverse Concert</h1>
-                  <p>details and pic</p>
-                <h1>Rockford RiverDays</h1>
-                  <p>details and pic</p>
-                <h1>June 2022 California Rodeo</h1>
-                  <p>details and pic</p>
-                <h1>Event</h1>
-                <h1>Event</h1>
-              </div>
-            </div>
-            <div class="user-card"></div>
-            <div className="user-column">
-              <h1>Glacier National Park</h1>
-                <p>details and pic</p>
-              <h1>Joshua Tree Park</h1>
-                <p>details and pic</p>
-              <h1>Itasca State Park</h1>
-                <p>details and pic</p>
-              <h1>Park</h1>
-              <h1>Park</h1>
-              <h1>Park</h1>
-            </div>
+          </div>
         </div>
     </div>
     );
