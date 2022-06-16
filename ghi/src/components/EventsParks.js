6ee3a96e7e4218939813bb2ed34beda616ec4e5d
Colton <coltstecker@gmail.com>
2022-06-15 19:04:14 -0500

    event data testing

--- ghi/src/components/EventsParks.js
+++ ghi/src/components/EventsParks.js
@@ -5,6 +5,10 @@ import React from 'react'
 function EventsParks(props) {
   let searchData = props.sData
   console.log("events and parks", searchData)
+  localStorage.setItem('data',JSON.stringify(searchData))
+  
+  const itemData = JSON.parse(localStorage.getItem('data'));
+  console.log("data", itemData)
     return (
       <div className="events-parks">
         <br />
