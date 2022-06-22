35caf4e5f507dbc30222719a3a7dc467502a3e1a
Kieran <kr292587@gmail.com>
2022-06-22 10:23:41 -0700

    filtering preferences

--- accounts/api/accounts_rest/acls.py
+++ accounts/api/accounts_rest/acls.py
@@ -24,6 +24,13 @@ def get_multiple_locations(request, city):
         # returns a response an array of objects to the front end 
         
  
+# def filter_preferences_for_events(event_content, preference_list):
+#     for event in event_content:
+#         for preference in preference_list:
+#             if event['type'] != preference:
+#                 event_content.pop(event)
+#     return event_content
+
  # Include a state parameter
 def get_events(request, lat, lon, state):
     if request.method == "GET":
@@ -35,6 +42,7 @@ def get_events(request, lat, lon, state):
         event_url = f"https://api.seatgeek.com/2/events?lat={lat}&lon={lon}&client_id={SEAT_GEEK_CLIENT_ID}"
         event_response = requests.get(event_url)
         event_content = json.loads(event_response.content)
+        # filtered_events = filter_preferences_for_events(event_content, user.preferences.all())
         abbr = convert_state_to_abbr(state)
         parks_url = f"https://developer.nps.gov/api/v1/parks?stateCode={abbr}&limit=5&api_key={NATIONAL_PARKS_API_KEY}"
         parks_response = requests.get(parks_url)
--- ghi/src/components/Search.js
+++ ghi/src/components/Search.js
@@ -43,6 +43,7 @@ class SearchBar extends React.Component {
                 "lon": this.state.selected_area.lon,
             }
             eventsAndParks['lat_lon'] = selected_lat_lon
+            console.log(eventsAndParks);
             this.props.sSData(eventsAndParks)
             localStorage.setItem('data',JSON.stringify(eventsAndParks))
         } 
