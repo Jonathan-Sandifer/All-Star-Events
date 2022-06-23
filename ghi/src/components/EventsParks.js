6d7b6610cff1420de3d72c2b617fa3b21b28b413
Jonathan Sandifer <jaysandifer5000@gmail.com>
2022-06-23 17:11:11 -0500

    Saving work

--- ghi/src/components/EventsParks.js
+++ ghi/src/components/EventsParks.js
@@ -1,124 +1,134 @@
-import React,{useState, useEffect} from 'react'
-
+import React, { useState, useEffect } from "react";
 
 function EventsParks(props) {
-  const itemData = JSON.parse(localStorage.getItem('data'));
-  const [weather, setWeather] = useState([])
-  const lat = itemData.lat_lon.lat
-  const lon = itemData.lat_lon.lon
-  const token = props.token 
+  const itemData = JSON.parse(localStorage.getItem("data"));
+  const [weather, setWeather] = useState([]);
+  const lat = itemData.lat_lon.lat;
+  const lon = itemData.lat_lon.lon;
+  const token = props.token;
 
   useEffect(() => {
     const fetchWeather = async () => {
-    const url = `http://localhost:8030/api/weather/${lat}/${lon}/`
-    const response = await fetch(url)
-    if (response.ok){
-      const weatherData = await response.json()
-      setWeather(weatherData)
-    }}; 
-    fetchWeather()
-  },[])
+      const url = `http://localhost:8030/api/weather/${lat}/${lon}/`;
+      const response = await fetch(url);
+      if (response.ok) {
+        const weatherData = await response.json();
+        setWeather(weatherData);
+      }
+    };
+    fetchWeather();
+  }, []);
 
- 
   async function saveEvent(e) {
-  
-    const event = JSON.parse(e.target.value)
-    const eventUrl = 'http://localhost:8080/api/saved_events/';
+    const event = JSON.parse(e.target.value);
+    const eventUrl = "http://localhost:8080/api/saved_events/";
     const fetchConfig = {
       method: "POST",
       body: JSON.stringify({
-        "name": event.title,
-	      "event_type": event.type,
-	      "city": event.venue.city,
-	      "venue_name": event.venue.name,
-	      "picture_url": event.performers[0].image
+        name: event.title,
+        event_type: event.type,
+        city: event.venue.city,
+        venue_name: event.venue.name,
+        picture_url: event.performers[0].image,
       }),
       headers: {
-        'Content-Type': 'application/json',
-        'authorization': `Bearer ${token}`
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
       },
-      credentials: "include"
+      credentials: "include",
     };
     const response = await fetch(eventUrl, fetchConfig);
     if (response.ok) {
-      console.log("SUCCCCESSSSSS")
+      console.log("SUCCCCESSSSSS");
     }
   }
-  
 
-  let icon = weather.main?weather.weather[0].icon: <></>
-  
-  
-    return (
-      <div className="events-parks">
-        <div className="weather">
-          Current Weather
-          <br/>
-          <div className="weather2">
-          {weather.main?weather.main.temp: <></>} °F
-          <br/>
-          {weather.main?weather.weather[0].description: <></>}
+  let icon = weather.main ? weather.weather[0].icon : <></>;
+
+  return (
+    <div className="events-parks">
+      <div className="weather">
+        Current Weather
+        <br />
+        <div className="weather2">
+          {weather.main ? weather.main.temp : <></>} °F
           <br />
-          <img  src ={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt='' />
-          </div>
+          {weather.main ? weather.weather[0].description : <></>}
+          <br />
+          <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
         </div>
-        <div className="row">
-            <div className="column">
-              <h1>Events</h1>   
-          {itemData.events.map(event => {
+      </div>
+      <div className="row">
+        <div className="column">
+          <h1>Events</h1>
+          {itemData.events.map((event) => {
             return (
               <div key={event.id + event.datetime_local}>
-              <>
-                <div>
-                  <img className="user-card-image" src={event.performers[0].image} alt=''/>
+                <>
+                  <div>
+                    <img
+                      className="user-card-image"
+                      src={event.performers[0].image}
+                      alt=""
+                    />
                   </div>
                   <div className="user-card-content">
                     <p>
-                    {event.title}
-                    <br/>
-                    {event.venue.name}
-                    <br/>
-                    {event.venue.city}
-                    <br/>
-                    {event.datetime_local}
-                    <br/>
-                    {event.type}
-                    <br/>
-                    <button className="user-card-info" value={JSON.stringify(event)} onClick = {saveEvent}>Save</button>
+                      {event.title}
+                      <br />
+                      {event.venue.name}
+                      <br />
+                      {event.venue.city}
+                      <br />
+                      {event.datetime_local}
+                      <br />
+                      {event.type}
+                      <br />
+                      <button
+                        className="user-card-info"
+                        value={JSON.stringify(event)}
+                        onClick={saveEvent}
+                      >
+                        Save
+                      </button>
                     </p>
                   </div>
                 </>
               </div>
             );
           })}
-            </div>
-            <div className="column">
-              <h1>Parks</h1>
-              {itemData.parks.map(park => {
+        </div>
+        <div className="column">
+          <h1>Parks</h1>
+          {itemData.parks.map((park) => {
             return (
               <div key={park.id}>
-              <>
-                <div>
-                  <img className="user-card-image" src={park.images[0].url} alt=''/>
+                <>
+                  <div>
+                    <img
+                      className="user-card-image"
+                      src={park.images[0].url}
+                      alt=""
+                    />
                   </div>
-                  <div className= "user-card-content">
+                  <div className="user-card-content">
                     <p>
-                    {park.fullName}
-                    <br/>
-                    {park.description}
-                    {park.states}
-                    <br/>
-                    <button className="user-card-info">Save</button>
+                      {park.fullName}
+                      <br />
+                      {park.description}
+                      {park.states}
+                      <br />
+                      <button className="user-card-info">Save</button>
                     </p>
                   </div>
                 </>
-                </div>
+              </div>
             );
           })}
-            </div>
         </div>
-            </div> 
-    );
-  }
-  
-  export default EventsParks;
+      </div>
+    </div>
+  );
+}
+
+export default EventsParks;
--- ghi/src/components/userEventsParks.js
+++ ghi/src/components/userEventsParks.js
@@ -1,62 +1,62 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function UserEventsParks(props) {
-
-  const token = props.token 
+  const token = props.token;
   // console.log("TOKEN!!!!@#@#@$", token)
-  const [savedEvent, setSavedEvents] = useState([])
+  const [savedEvent, setSavedEvents] = useState([]);
 
   useEffect(() => {
     if (token) {
-        const fetchSavedEvents = async () => {
-        const url = `http://localhost:8080/api/show_events/`
+      const fetchSavedEvents = async () => {
+        const url = `http://localhost:8080/api/show_events/`;
         const response = await fetch(url, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
-        if (response.ok){
-          const savedEventsData = await response.json()
-          setSavedEvents(savedEventsData)
-        }}; 
-        fetchSavedEvents()
+        if (response.ok) {
+          const savedEventsData = await response.json();
+          setSavedEvents(savedEventsData);
+        }
+      };
+      fetchSavedEvents();
+    } else {
+      console.log("sucks to suck");
     }
-    else {
-      console.log("sucks to suck")
-    }
- 
-  },[token])
-  console.log("!!!!!!!!", savedEvent)
+  }, [token]);
+  console.log("!!!!!!!!", savedEvent);
   return (
-    
     <div className="user-events-parks">
-        <div className="column">
-
-        {savedEvent.map(event => {
-            return (
-                <div key={event.event.id}>
-                <>
-                  <div>
-                    <img className="user-card-image" src={event.event.picture_url} alt=''/>
-                    </div>
-                    <div className="user-card-content">
-                      <p>
-                      {event.event.name}
-                      <br/>
-                      {event.event.venue_name}
-                      <br/>
-                      {event.event.city}
-                      <br/>
-                      {event.event.event_type}
-                      </p>
-                    </div>
-                  </>
+      <div className="column">
+        {savedEvent.map((event) => {
+          return (
+            <div key={event.event.id}>
+              <>
+                <div>
+                  <img
+                    className="user-card-image"
+                    src={event.event.picture_url}
+                    alt=""
+                  />
                 </div>
-              );
-            })}
-              </div>
-              </div>
-              );
-  }
+                <div className="user-card-content">
+                  <p>
+                    {event.event.name}
+                    <br />
+                    {event.event.venue_name}
+                    <br />
+                    {event.event.city}
+                    <br />
+                    {event.event.event_type}
+                  </p>
+                </div>
+              </>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default UserEventsParks;
