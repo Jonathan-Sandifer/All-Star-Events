86385b414ea330aa2faa4ba06feb10b076e2808e
Shane <ssiebler21@gmail.com>
2022-06-23 15:24:12 -0600

    got the saved events populated on the user events/parks page

--- ghi/src/components/userEventsParks.js
+++ ghi/src/components/userEventsParks.js
@@ -17,7 +17,6 @@ function UserEventsParks(props) {
         });
         if (response.ok){
           const savedEventsData = await response.json()
-          console.log("????????", savedEventsData)
           setSavedEvents(savedEventsData)
         }}; 
         fetchSavedEvents()
@@ -31,38 +30,33 @@ function UserEventsParks(props) {
   return (
     
     <div className="user-events-parks">
-      <div className="user-cards">
-        <div className="user-card">
+        <div className="column">
+
         {savedEvent.map(event => {
             return (
-              <div key={event.id}>
-              <>
-              {event.event.name}
-              {event.event.venue_name}
-              {event.event.event_type}
-              {event.event.city}
-
-                </>
+                <div key={event.event.id}>
+                <>
+                  <div>
+                    <img className="user-card-image" src={event.event.picture_url} alt=''/>
+                    </div>
+                    <div className="user-card-content">
+                      <p>
+                      {event.event.name}
+                      <br/>
+                      {event.event.venue_name}
+                      <br/>
+                      {event.event.city}
+                      <br/>
+                      {event.event.event_type}
+                      </p>
+                    </div>
+                  </>
+                </div>
+              );
+            })}
               </div>
-            );
-          })}
-          <img
-            className="user-card-image"
-            src="https://fakeimg.pl/400x300/009578/fff/"
-            alt=""
-          />
-          <div className="user-card-content">
-            
-            <p>
-              Alias itaque praesentium eum, consequatur ducimus asperiores
-              accusantium velit minima?
-            </p>
-            <div className="user-card-content"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+              </div>
+              );
+  }
 
 export default UserEventsParks;
