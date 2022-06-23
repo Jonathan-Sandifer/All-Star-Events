a1d24ef0fbad039f29f98d462d72843725e35bc8
Jonathan Sandifer <jaysandifer5000@gmail.com>
2022-06-23 18:13:38 -0500

    Saving

--- parks/api/parks_rest/urls.py
+++ parks/api/parks_rest/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import protected_view, public_view, save_parks
+from .views import protected_view, public_view
 
 urlpatterns = [
     path("protected/", protected_view, name="protected_view"),
-    path("public/", public_view, name="public_view"),
-    path("saved_events/", save_parks, name="saved_events"),
+    path("public/", public_view, name="public_view")
 ]
